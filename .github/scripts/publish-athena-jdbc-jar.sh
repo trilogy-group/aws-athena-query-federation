8eb01f0c45f88132432d85d47b89724c2050aee9
Milan Trninic <milan.trninic@aurea.com>
2022-01-14 10:38:44 +0100

    Update workflow

--- .github/scripts/publish-athena-jdbc-jar.sh
+++ .github/scripts/publish-athena-jdbc-jar.sh
@@ -3,14 +3,14 @@
 #sudo apt-get update --yes
 #sudo apt-get install maven --yes
 
-#cd athena-federation-sdk
-#mvn clean install
+cd athena-federation-sdk
+mvn clean install -Dmaven.test.skip
 
-#cd athena-federation-integ-test
-#mvn clean install
+cd athena-federation-integ-test
+mvn clean install -Dmaven.test.skip
 
 cd athena-jdbc
-#mvn clean install
+mvn clean install -Dmaven.test.skip
 mvn package -Dmaven.test.skip
 
 mv ./athena-jdbc/target/athena-jdbc-*.jar ./athena-jdbc/target/athena-jdbc.jar
